b1277539d56a939c1f870e734c750ae740fe7c1e
Den Elfie <denelfie@gmail.com>
2021-04-28 11:32:06 +0200

    batch file error fix

--- windows/mining.bat
+++ windows/mining.bat
@@ -1,7 +1,7 @@
 start "" "C:\Ethminer\init-miningconfig"
 
 :start
-ethminer -U -R -P stratum://0x6DFcFe20565729f9Db800bc7342b62B3D2eC864B.lazy-reader@eu1.ethermine.org:4444
+ethminer -U -R -P stratum://0x6DFcFe20565729f9Db800bc7342b62B3D2eC864B.lazyreader@eu1.ethermine.org:4444
 
 @ping -n 10 localhost> nul
 
