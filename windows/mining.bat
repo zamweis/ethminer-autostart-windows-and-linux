2428a6fa5da0799840e6728944d6f563e35f5d57
Den Elfie <denelfie@gmail.com>
2021-04-28 11:36:46 +0200

    batch file comments

--- windows/mining.bat
+++ windows/mining.bat
@@ -1,8 +1,8 @@
 :: Shortcut to init the config for your gpu with msi-afterburner
 start "" "C:\Ethminer\init-miningconfig"
 
-:: Start ethminer
 :start
+:: Start ethminer
 ethminer -U -R -P stratum://0x6DFcFe20565729f9Db800bc7342b62B3D2eC864B.lazyreader@eu1.ethermine.org:4444
 
 :: Wait 10 seconds before restarting ethminer if crashed
